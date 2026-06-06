77b70133351df4270b2d3364188d73af064b3468
krishna-mk-0124 <krishna0124@gmail.com>
2026-06-06 21:14:20 +0530

    Fix useState ReferenceError in SceneContainer

--- components/canvas/SceneContainer.tsx
+++ components/canvas/SceneContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
